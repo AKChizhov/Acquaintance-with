520f5463af8a2408ec9de64dd4c3a8c51c0cb133
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-15 19:42:21 +0300

    EXAMPLE№_006 с особым приветствием определенного пользователя

---
+++ EXAMPLE№_006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ведите имя пользователя :  ");
+string? userName = Console.ReadLine();
+
+if (userName?.ToLower() == "федя")
+{
+    Console.WriteLine("Ура, это же Федя  !!");
+}
+else
+{
+    Console.WriteLine("Привет,  "+ userName);
+}
+
+
